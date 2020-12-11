76c9d24523b0eb3241f03b930656ea3817624cb4
Tedis32 <teddrinkscoffee@gmail.com>
2020-12-11 16:05:17 +0000

    Update VendingGUI.java

--- src/gui/VendingGUI.java
+++ src/gui/VendingGUI.java
@@ -62,12 +62,9 @@ public class VendingGUI extends Application {
 		gPane.add(rbShowProducts,0,0);
 		gPane.add(rbBuy,1,0);
 		gPane.add(rbInsertCoins,0,2);
-		gPane.add(rbReturnCoins,1,2);
-		
+		gPane.add(rbReturnCoins,1,2);	
 		gPane.add(rbAddCredit,2,2);
-		
 		gPane.add(rbOpFuntions,0,4);
-		
 		gPane.add(rbQuit,1,4);
 	
 		ToggleGroup group = new ToggleGroup();
